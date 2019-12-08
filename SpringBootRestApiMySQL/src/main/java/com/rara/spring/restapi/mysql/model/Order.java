cdacfb943e9500bf4553d37a4db6be2b59735ddf
radu931 <13164025+radu931@users.noreply.github.com>
2019-12-08 15:35:01 +0100

    deleted OrderProduct.java and OrderProductPK.java changes to Order.java for final implementation

--- SpringBootRestApiMySQL/src/main/java/com/rara/spring/restapi/mysql/model/Order.java
+++ SpringBootRestApiMySQL/src/main/java/com/rara/spring/restapi/mysql/model/Order.java
@@ -2,11 +2,14 @@ package com.rara.spring.restapi.mysql.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -16,64 +19,75 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    private LocalDate dateCreated;
-
-    private String status;
-
-    @JsonManagedReference
-    @OneToMany(mappedBy = "pk.order")
-    @Valid
-    private List<OrderProduct> orderProducts = new ArrayList<>();
-
-    @Transient
-    public Double getTotalOrderPrice() {
-        double sum = 0D;
-        List<OrderProduct> orderProducts = getOrderProducts();
-        for (OrderProduct op : orderProducts) {
-            sum += op.getTotalPrice();
-        }
-        return sum;
-    }
-
-    @Transient
-    public int getNumberOfProducts() {
-        return this.orderProducts.size();
-    }
-
-    // standard getters and setters
+    private Date deliveryDate;
 
 
-    public Long getId() {
-        return id;
-    }
+//
+//    @JsonFormat(pattern = "dd/MM/yyyy")
+//    private LocalDate dateCreated;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    private String status;
 
-    public LocalDate getDateCreated() {
-        return dateCreated;
-    }
+    @OneToMany
+    @OnDelete(action =  OnDeleteAction.CASCADE)
+    private List<Product> orderProducts = new ArrayList<>();
 
-    public void setDateCreated(LocalDate dateCreated) {
-        this.dateCreated = dateCreated;
-    }
+    @ManyToOne
+    @OnDelete(action =  OnDeleteAction.CASCADE)
+    private Customer customer;
 
-    public String getStatus() {
-        return status;
-    }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
 
-    public List<OrderProduct> getOrderProducts() {
-        return orderProducts;
-    }
 
-    public void setOrderProducts(List<OrderProduct> orderProducts) {
-        this.orderProducts = orderProducts;
-    }
+    public Order(){}
+
+//    @Transient
+//    public Double getTotalOrderPrice() {
+//        double sum = 0D;
+//        List<OrderProduct> orderProducts = getOrderProducts();
+//        for (OrderProduct op : orderProducts) {
+//            sum += op.getTotalPrice();
+//        }
+//        return sum;
+//    }
+//
+//    @Transient
+//    public int getNumberOfProducts() {
+//        return this.orderProducts.size();
+//    }
+//
+//    // standard getters and setters
+//
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public LocalDate getDateCreated() {
+//        return dateCreated;
+//    }
+//
+//    public void setDateCreated(LocalDate dateCreated) {
+//        this.dateCreated = dateCreated;
+//    }
+//
+//    public String getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(String status) {
+//        this.status = status;
+//    }
+//
+//    public List<OrderProduct> getOrderProducts() {
+//        return orderProducts;
+//    }
+//
+//    public void setOrderProducts(List<OrderProduct> orderProducts) {
+//        this.orderProducts = orderProducts;
+//    }
 }
