05d94daf910869360c7f2360f454c4c578a4c79c
radu931 <13164025+radu931@users.noreply.github.com>
2019-12-15 12:34:00 +0100

    Angular Add Product function.

--- client-Angular8SpringBoot/Angular8SpringBoot/src/app/create-product/create-product.component.ts
+++ client-Angular8SpringBoot/Angular8SpringBoot/src/app/create-product/create-product.component.ts
@@ -1,11 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-	selector: 'create-product',
-	templateUrl: 'create-product.component.html'
-})
+import { Product } from '../models/product';
+import { ProductService } from '../services/product.service';
 
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.css']
+})
 export class CreateProductComponent implements OnInit {
 
-	ngOnInit() { }
+  product: Product = new Product();
+  submitted = false;
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+  }
+
+  newProduct(): void {
+    this.submitted = false;
+    this.product = new Product();
+  }
+
+  save() {
+    this.productService.createProduct(this.product)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.submitted = true;
+        },
+        error => console.log(error));
+    this.product = new Product();
+  }
+
+  onSubmit() {
+    this.save();
+  }
+
 }
